c9ade472b216e730e27e7c588e92ead329b915e0
unknown <lairtondasilva07@gmail.com>
2022-05-15 23:43:41 -0300

    criado alguns cabecalhos para parte visual

---
+++ Projeto1GenerationBrasil/src/exibicao/Cabecalhos.java
@@ -0,0 +1,34 @@
+package exibicao;
+
+public class Cabecalhos {
+	void textoCadastrar() {
+		System.out.println("                _           _                  ");
+		System.out.println("               | |         | |                 ");
+		System.out.println("   ___ __ _  __| | __ _ ___| |_ _ __ __ _ _ __ ");
+		System.out.println("  / __/ _` |/ _` |/ _` / __| __| '__/ _` | '__|");
+		System.out.println(" | (_| (_| | (_| | (_| \\__ \\ |_| | | (_| | |   ");
+		System.out.println("  \\___\\__,_|\\__,_|\\__,_|___/\\__|_|  \\__,_|_|   \n\n");
+	}
+	void textoLogar() {
+		System.out.println("  _                        ");
+		System.out.println(" | |                       ");
+		System.out.println(" | | ___   __ _  __ _ _ __ ");
+		System.out.println(" | |/ _ \\ / _` |/ _` | '__|");
+		System.out.println(" | | (_) | (_| | (_| | |   ");
+		System.out.println(" |_|\\___/ \\__, |\\__,_|_|   ");
+		System.out.println("           __/ |           ");
+		System.out.println("          |___/            ");
+	}
+	
+	void textoSejaBemvinde() {
+		System.out.println("   _____      _         ____                         _           _       ");
+		System.out.println("  / ____|    (_)       |  _ \\                       (_)         | |     ");
+		System.out.println(" | (___   ___ _  __ _  | |_) | ___ _ __ ___   __   ___ _ __   __| | ___ ");
+		System.out.println("  \\___ \\ / _ \\ |/ _` | |  _ < / _ \\ '_ ` _ \\  \\ \\ / / | '_ \\ / _` |/ _ \\");
+		System.out.println("  ____) |  __/ | (_| | | |_) |  __/ | | | | |  \\ V /| | | | | (_| |  __/");
+		System.out.println(" |_____/ \\___| |\\__,_| |____/ \\___|_| |_| |_|   \\_/ |_|_| |_|\\__,_|\\___|");
+		System.out.println("            _/ |                                                        ");
+		System.out.println("           |__/                                                         ");
+	}
+	
+}
---
+++ Projeto1GenerationBrasil/src/exibicao/Exibir.java
@@ -0,0 +1,13 @@
+package exibicao;
+
+public class Exibir {
+
+	public static void main(String[] args) {
+		Cabecalhos cabecalho = new Cabecalhos();
+		cabecalho.textoCadastrar();
+		cabecalho.textoLogar();
+		cabecalho.textoSejaBemvinde();
+
+	}
+
+}
